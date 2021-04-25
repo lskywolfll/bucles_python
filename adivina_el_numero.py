161e155054797c7b8874e42a81a41d927dc8bd52
Rene <renealejandrosanchezmorales@gmail.com>
2021-04-25 17:58:41 -0400

    se termina juego de adivinar el numero

---
+++ adivina_el_numero.py
@@ -0,0 +1,34 @@
+import random
+from functions.inputs import *
+
+
+def numeroAleatorio():
+    return random.randint(1, 100)
+
+
+def distanciaObjetivo(numero, objetivo):
+    if numero < objetivo:
+        print("Busca un número más grande")
+    elif numero > objetivo:
+        print("Busca un número más pequeño")
+
+
+def game(numero):
+    objetivo = numeroAleatorio()
+
+    while numero != objetivo:
+        distanciaObjetivo(numero, objetivo)
+        numero = get_input(
+            int, "Elige un número del 1 al 100: ", "un número valido")
+        if numero == objetivo:
+            print("¡Ganaste!")
+
+
+def run():
+    numero = get_input(
+        int, "Elige un número del 1 al 100: ", "un número valido")
+    game(numero)
+
+
+if __name__ == '__main__':
+    run()
